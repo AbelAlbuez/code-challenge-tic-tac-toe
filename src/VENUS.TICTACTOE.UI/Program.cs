4ce63f4068de42e8d5652a98b292248da1221945
Abel Albuez <aalbuez@hchb.com>
2023-02-22 09:49:28 -0400

    Update project structure

---
+++ src/VENUS.TICTACTOE.UI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VENUS.TICTACTOE.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
